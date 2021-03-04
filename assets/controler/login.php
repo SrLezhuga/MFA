2a456fa4b2bda5abb130d5a1705beaea6b3d78fc
SrLezhuga <58528751+SrLezhuga@users.noreply.github.com>
2021-03-04 14:18:25 -0600

    Update login.php

--- assets/controler/login.php
+++ assets/controler/login.php
@@ -1,41 +1,23 @@
 <?php
-$recaptcha_secret = '6Len7XEaAAAAAGTEcowp6Nftnx7znchBD7rEK4f6';
-$recaptcha_response = $_POST['recaptcha_response'];
-$url = 'https://www.google.com/recaptcha/api/siteverify';
+include "conexion.php";
 
-$data = array('secret' => $recaptcha_secret, 'response' => $recaptcha_response, 'remoteip' => $_SERVER['REMOTE_ADDR']);
-$curlConfig = array(CURLOPT_URL => $url, CURLOPT_POST => true, CURLOPT_RETURNTRANSFER => true, CURLOPT_POSTFIELDS => $data);
-$ch = curl_init();
-curl_setopt_array($ch, $curlConfig);
-$response = curl_exec($ch);
-curl_close($ch);
+$user      = $_POST['formUser'];
+$password  = $_POST['formPass'];
 
-$jsonResponse = json_decode($response);
-if ($jsonResponse->success === true) {
-    include "conexion.php";
+$encry = sha1($password);
+$sql        =  "SELECT Priv FROM tab_login WHERE User= '$user' AND Pass = '$encry'";
 
-    $user      = $_POST['formUser'];
-    $password  = $_POST['formPass'];
+if ($con->query($sql)) {
 
-    $encry = sha1($password);
-    $sql        =  "SELECT Priv FROM tab_login WHERE User= '$user' AND Pass = '$encry'";
+    $query      =  $con->query($sql);
+    $rs         =  $query->fetch_array();
+    $priv_user  =  $rs['Priv'];
 
-    if ($con->query($sql)) {
-
-        $query      =  $con->query($sql);
-        $rs         =  $query->fetch_array();
-        $priv_user  =  $rs['Priv'];
-
-        if ($priv_user == "Admin") {
-            header("Location: https://mayoreoferreteroatlas.com/mfa/Administrar");
-        } elseif ($priv_user == "RH") {
-            header("Location: https://mayoreoferreteroatlas.com/mfa/Reclutamiento");
-        } else {
-            header("Location: https://mayoreoferreteroatlas.com/mfa/500");
-        }
+    if ($priv_user == "Admin") {
+        header("Location: https://mayoreoferreteroatlas.com/mfa/Administrar");
+    } elseif ($priv_user == "RH") {
+        header("Location: https://mayoreoferreteroatlas.com/mfa/Reclutamiento");
+    } else {
+        header("Location: https://mayoreoferreteroatlas.com/mfa/500");
     }
-
-    mysqli_close($con);
-} else {
-    header("Location: https://mayoreoferreteroatlas.com/mfa/404");
 }
