07d4defab422ad3c68074ee911b641cad0dc5f44
SrLezhuga <58528751+SrLezhuga@users.noreply.github.com>
2021-03-04 12:05:17 -0600

    Update conexion.php

--- assets/controler/conexion.php
+++ assets/controler/conexion.php
@@ -1,17 +1,11 @@
 <?php
 
-//PRODUCCION
-/*$host="";
-$user="";
-$password="";
-$db="";*/
-
-//LOCAL
 $host     = "localhost";
 $user     = "mayoreof_Mfa_2021";
 $password = "mPH7#)So[4n%";
 $db       = "mayoreof_mfa_web";
 
+/*
 $con = new mysqli($host, $user, $password, $db) or die("No se ha podido conectar al servidor de Base de datos");
 mysqli_set_charset($con, 'utf8');
 $db = mysqli_select_db($con, $db) or die("Upps! Pues va a ser que no se ha podido conectar a la base de datos de mayoreo");
@@ -19,4 +13,16 @@ $con->query("set names utf8");
 
 $base_url="localhost";
 
-?>
+*/
+
+$con=mysqli_connect('127.0.0.1',$user,$password,$db);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+else{
+
+  echo "conexion correcta";
+
+}
