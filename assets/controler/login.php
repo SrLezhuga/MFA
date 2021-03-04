23443383db77d12476d4d60ca271a2eadd565980
SrLezhuga <58528751+SrLezhuga@users.noreply.github.com>
2021-03-04 14:09:51 -0600

    Update login.php

--- assets/controler/login.php
+++ assets/controler/login.php
@@ -1,43 +1,45 @@
-
 <?php
 
-$recaptcha_secret = '6Len7XEaAAAAAGTEcowp6Nftnx7znchBD7rEK4f6';
-$recaptcha_response = $_POST['recaptcha_response'];
-$url = 'https://www.google.com/recaptcha/api/siteverify';
+if (isset($_POST['action']) && ($_POST['action'] == 'process')) {
 
-$data = array('secret' => $recaptcha_secret, 'response' => $recaptcha_response, 'remoteip' => $_SERVER['REMOTE_ADDR']);
-$curlConfig = array(CURLOPT_URL => $url, CURLOPT_POST => true, CURLOPT_RETURNTRANSFER => true, CURLOPT_POSTFIELDS => $data);
-$ch = curl_init();
-curl_setopt_array($ch, $curlConfig);
-$response = curl_exec($ch);
-curl_close($ch);
+    $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify'; 
+    $recaptcha_secret = '6Len7XEaAAAAAGTEcowp6Nftnx7znchBD7rEK4f6'; 
+    $recaptcha_response = $_POST['recaptcha_response']; 
+    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response); 
+    $recaptcha = json_decode($recaptcha); 
+    
+    if($recaptcha->score >= 0.7){
+    
+        include "conexion.php";
 
-$jsonResponse = json_decode($response);
-if ($jsonResponse->success === true) {
-    include "conexion.php";
-
-    $user      = $_POST['formUser'];
-    $password  = $_POST['formPass'];
-
-    $encry = sha1($password);
-    $sql        =  "SELECT Priv FROM tab_login WHERE User= '$user' AND Pass = '$encry'";
-
-    if ($con->query($sql)) {
-
-        $query      =  $con->query($sql);
-        $rs         =  $query->fetch_array();
-        $priv_user  =  $rs['Priv'];
-
-        if ($priv_user == "Admin") {
-            header("Location: ../../Administrar");
-        } elseif ($priv_user == "RH") {
-            header("Location: ../../Reclutamiento");
-        } else {
-            header("Location: ../../500");
+        $user      = $_POST['formUser'];
+        $password  = $_POST['formPass'];
+    
+        $encry = sha1($password);
+        $sql        =  "SELECT Priv FROM tab_login WHERE User= '$user' AND Pass = '$encry'";
+    
+        if ($con->query($sql)) {
+    
+            $query      =  $con->query($sql);
+            $rs         =  $query->fetch_array();
+            $priv_user  =  $rs['Priv'];
+    
+            if ($priv_user == "Admin") {
+                header("Location: ../../Administrar");
+            } elseif ($priv_user == "RH") {
+                header("Location: ../../Reclutamiento");
+            } else {
+                header("Location: ../../500");
+            }
         }
+    
+        mysqli_close($con);
+    
+    } else {
+    
+        header("Location: ../../404");
+    
+    }
+    
     }
 
-    mysqli_close($con);
-} else {
-    header("Location: ../../404");
-}
