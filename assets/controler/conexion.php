e6d4d9878f7ab86f16d09b190bdfcb84ba380465
SrLezhuga <58528751+SrLezhuga@users.noreply.github.com>
2021-03-04 12:29:39 -0600

    Update conexion a la base
    Conexion a la base

--- assets/controler/conexion.php
+++ assets/controler/conexion.php
@@ -4,15 +4,13 @@ $host     = "localhost";
 $user     = "mayoreof_Mfa_2021";
 $password = "mPH7#)So[4n%";
 $db       = "mayoreof_mfa_web";
+$base_url="localhost";
 
 /*
 $con = new mysqli($host, $user, $password, $db) or die("No se ha podido conectar al servidor de Base de datos");
 mysqli_set_charset($con, 'utf8');
 $db = mysqli_select_db($con, $db) or die("Upps! Pues va a ser que no se ha podido conectar a la base de datos de mayoreo");
 $con->query("set names utf8");
-
-$base_url="localhost";
-
 */
 
 $con=mysqli_connect('127.0.0.1',$user,$password,$db);
