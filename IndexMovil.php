37929eae6b408acbede48d70b0ca58b395bc2cf0
SrLezhuga <58528751+SrLezhuga@users.noreply.github.com>
2021-03-09 13:11:09 -0600

    Update IndexMovil.php

--- IndexMovil.php
+++ IndexMovil.php
@@ -115,6 +115,13 @@
 								</div>
 							</a>
 						</div>
+						<input type="button" value="Say hello" onClick="showAndroidToast('Hello Android!')" />
+
+						<script type="text/javascript">
+							function showAndroidToast(toast) {
+								Android.showToast(toast);
+							}
+						</script>
 					</div>
 					<br>
 				</div>
