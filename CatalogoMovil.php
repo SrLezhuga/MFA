dfc7e922b100e60776c647973d03a42ca35b6be0
SrLezhuga <58528751+SrLezhuga@users.noreply.github.com>
2021-03-11 12:47:45 -0600

    Update CatalogoMovil.php

--- CatalogoMovil.php
+++ CatalogoMovil.php
@@ -15,8 +15,6 @@
 		<!-- Content -->
 		<div class="content">
 
-
-
 			<!-- Section 1 -->
 			<div class="section-1-container " id="section-1">
 				<div class="container">
@@ -42,7 +40,7 @@
 							';
 
 							?>
-							<div class="carousel" style="background: transparent;" data-flickity='{ "draggable": true,
+								<div class="carousel" style="background: transparent;" data-flickity='{ "draggable": true,
 									"autoPlay": true,
 									"bgLazyLoad": true,
 									"autoPlay": 2000,
@@ -52,8 +50,8 @@
 									"prevNextButtons": true,
 									"pageDots": false}'>
 
-							<?php
-								$queryCatalogo = "SELECT * FROM tab_catalogo WHERE Sucursal = '". $Sucursal['Sucursal'] ."'";
+								<?php
+								$queryCatalogo = "SELECT * FROM tab_catalogo WHERE Sucursal = '" . $Sucursal['Sucursal'] . "'";
 								$rsCatalogo = mysqli_query($con, $queryCatalogo) or die("Error de consulta");
 								while ($Catalogo = mysqli_fetch_array($rsCatalogo)) {
 									echo '
@@ -63,35 +61,35 @@
 										</div>
 										';
 								}
-							echo '
+								echo '
 							</div>
 							';
 							}
-							?>
-						</div>
+								?>
+								</div>
 
+						</div>
+						<br>
 					</div>
-					<br>
 				</div>
+
 			</div>
+			<br>
 
 		</div>
-		<br>
+		<!-- End content -->
 
-	</div>
-	<!-- End content -->
+		<div class="plus-button"></div>
 
-	<div class="plus-button"></div>
-
-	<?php
-	$querySocial = "SELECT * FROM tab_social";
-	$rsSocial = mysqli_query($con, $querySocial) or die("Error de consulta");
-	while ($Social = mysqli_fetch_array($rsSocial)) {
-		echo '
+		<?php
+		$querySocial = "SELECT * FROM tab_social";
+		$rsSocial = mysqli_query($con, $querySocial) or die("Error de consulta");
+		while ($Social = mysqli_fetch_array($rsSocial)) {
+			echo '
         <div class="social-button ' . $Social['red_social'] . '-button"><a data-toggle="tooltip" title="' . ucfirst($Social['red_social']) . '" data-placement="left" href="' . $Social['Url_social'] . '" target="_blank"><i class="fab fa-' . $Social['red_social'] . '"></i></a></div>
     ';
-	}
-	?>
+		}
+		?>
 </body>
 
 </html>
