0b742100c9cc124d9fb2d7b70ca68b5fae7f784f
SrLezhuga <58528751+SrLezhuga@users.noreply.github.com>
2021-03-20 12:01:16 -0600

    Update alta.php

--- assets/controler/vacante/alta.php
+++ assets/controler/vacante/alta.php
@@ -5,13 +5,13 @@ $vacante = $_POST['Vacante'];
 
 // Consulta segura para evitar inyecciones SQL.
 
-$sql = "INSERT INTO tab_vacantes VALUES(NULL, '$vacante', '','','')";
+$sql = "INSERT INTO tab_vacantes VALUES(NULL, '$vacante', '','','','true')";
 if (mysqli_query($con, $sql)) {
     echo 0;
 }else {
     echo mysqli_error($con);
 }
-
+ 
 mysqli_close($con);
 
 ?>
