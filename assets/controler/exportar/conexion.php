c6dbd7773c22f348341c2bfccbfe0e1f7505582f
SrLezhuga <58528751+SrLezhuga@users.noreply.github.com>
2021-03-26 12:44:16 -0600

    update excel

--- assets/controler/exportar/conexion.php
+++ assets/controler/exportar/conexion.php
@@ -2,19 +2,12 @@
 function conectar()
 {
     $servidor = "localhost";
-    $usuario = "root";
-    $contra = "";
-    $db = "mfa_web";
+    $usuario = "mayoreof_Mfa_2021";
+    $contra = "mPH7#)So[4n%";
+    $db = "mayoreof_mfa_web";
 
     $conexion = new mysqli($servidor, $usuario, $contra, $db);
     $conexion->set_charset("UTF-8");
 
     return $conexion;
 }
-/*
-$host     = "localhost";
-$user     = "root";
-$password = "";
-$db       = "mfa_web";
-$base_url = "localhost";
-*/
